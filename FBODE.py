4b6a3c6c327dfb5aaa25c05cde942f5624ae70a4
RachelZhou287 <157051444+RachelZhou287@users.noreply.github.com>
2025-10-31 14:22:51 -0500

    Refactor driver_P and driver_U to use TensorFlow

--- FBODE.py
+++ FBODE.py
@@ -1,3 +1,4 @@
+# FBODE.py
 import tensorflow as tf
 import numpy as np
 
@@ -16,29 +17,62 @@ class FBODEEquation(object):
         self.g = 0.2
         self.Delta_t = Delta_t
 
+    #def driver_P(self, Z_empirical, P, ALPHA, beta_vector):
+        #n_samples =    tf.shape(P)[0]
+        #driver =       tf.shape(P)[0]
+        #driver[:,0] =  tf.reshape(-beta_vector * ALPHA * Z_empirical * tf.reshape(P[:,0],[n_samples,1]) + self.kappa * tf.reshape(P[:,2],[n_samples,1]), [n_samples])
+        #driver[:,1] =  tf.reshape(beta_vector * ALPHA * Z_empirical * tf.reshape(P[:,0],[n_samples,1]) - self.gamma * tf.reshape(P[:,1],[n_samples,1]), [n_samples])
+        #driver[:,2] =  tf.reshape(self.gamma * tf.reshape(P[:,1],[n_samples,1]) - self.kappa * tf.reshape(P[:,2],[n_samples,1]), [n_samples])
+        #return driver
+
     def driver_P(self, Z_empirical, P, ALPHA, beta_vector):
-        n_samples =    tf.shape(P)[0]
-        Nstates =      tf.shape(P)[1]
-        driver =       np.zeros((n_samples, Nstates))
-        driver[:,0] =  tf.reshape(-beta_vector * ALPHA * Z_empirical * tf.reshape(P[:,0],[n_samples,1]) + self.kappa * tf.reshape(P[:,2],[n_samples,1]), [n_samples])
-        driver[:,1] =  tf.reshape(beta_vector * ALPHA * Z_empirical * tf.reshape(P[:,0],[n_samples,1]) - self.gamma * tf.reshape(P[:,1],[n_samples,1]), [n_samples])
-        driver[:,2] =  tf.reshape(self.gamma * tf.reshape(P[:,1],[n_samples,1]) - self.kappa * tf.reshape(P[:,2],[n_samples,1]), [n_samples])
-        return driver
+        # P: shape [n_samples, Nstates] (tf.Tensor)
+        n_samples = tf.shape(P)[0]
+        # Build driver tensor of same shape as P
+        driver = tf.zeros_like(P, dtype=tf.float64)
+    
+        P0 = tf.reshape(P[:, 0], [n_samples, 1])
+        P1 = tf.reshape(P[:, 1], [n_samples, 1])
+        P2 = tf.reshape(P[:, 2], [n_samples, 1])
+    
+        # compute each column as column vectors then concat
+        col0 = -beta_vector * ALPHA * Z_empirical * P0 + self.kappa * P2
+        col1 = beta_vector * ALPHA * Z_empirical * P0 - self.gamma * P1
+        col2 = self.gamma * P1 - self.kappa * P2
+    
+        driver = tf.concat([tf.reshape(col0, [n_samples, 1]),
+                            tf.reshape(col1, [n_samples, 1]),
+                            tf.reshape(col2, [n_samples, 1])], axis=1)
+        return tf.cast(driver, tf.float64)
 
-
+    #def driver_U(self, Z_empirical, U, ALPHA, beta_vector):
+        #n_samples =    tf.shape(U)[0]
+        #Nstates =      tf.shape(U)[1]
+        #driver =       np.zeros((n_samples, Nstates))
+        #driver[:,0] =   tf.reshape(beta_vector* ALPHA * Z_empirical * (tf.reshape(U[:,0],[n_samples,1])-tf.reshape(U[:,1],[n_samples,1])) - \
+                       #0.5 * self.cost_lambda1 * ((self.lambda1-ALPHA)**2), [n_samples])
+        #driver[:,1] =   tf.reshape(self.gamma * (tf.reshape(U[:,1],[n_samples,1])-tf.reshape(U[:,2],[n_samples,1])) - self.cost_I, [n_samples])
+        #driver[:,2] =   tf.reshape(self.kappa * (tf.reshape(U[:,2],[n_samples,1])-tf.reshape(U[:,0],[n_samples,1])), [n_samples])
+        #return driver
     def driver_U(self, Z_empirical, U, ALPHA, beta_vector):
-        n_samples =    tf.shape(U)[0]
-        Nstates =      tf.shape(U)[1]
-        driver =       np.zeros((n_samples, Nstates))
-        driver[:,0] =   tf.reshape(beta_vector* ALPHA * Z_empirical * (tf.reshape(U[:,0],[n_samples,1])-tf.reshape(U[:,1],[n_samples,1])) - \
-                       0.5 * self.cost_lambda1 * ((self.lambda1-ALPHA)**2), [n_samples])
-        driver[:,1] =   tf.reshape(self.gamma * (tf.reshape(U[:,1],[n_samples,1])-tf.reshape(U[:,2],[n_samples,1])) - self.cost_I, [n_samples])
-        driver[:,2] =   tf.reshape(self.kappa * (tf.reshape(U[:,2],[n_samples,1])-tf.reshape(U[:,0],[n_samples,1])), [n_samples])
-        return driver
+        n_samples = tf.shape(U)[0]
+    
+        U0 = tf.reshape(U[:, 0], [n_samples, 1])
+        U1 = tf.reshape(U[:, 1], [n_samples, 1])
+        U2 = tf.reshape(U[:, 2], [n_samples, 1])
+    
+        term0 = beta_vector * ALPHA * Z_empirical * (U0 - U1) - 0.5 * self.cost_lambda1 * ((self.lambda1 - ALPHA)**2)
+        term1 = self.gamma * (U1 - U2) - self.cost_I
+        term2 = self.kappa * (U2 - U0)
+    
+        driver = tf.concat([tf.reshape(term0, [n_samples, 1]),
+                            tf.reshape(term1, [n_samples, 1]),
+                            tf.reshape(term2, [n_samples, 1])], axis=1)
+        return tf.cast(driver, tf.float64)
 
 
 
     def optimal_ALPHA(self, U, Z_empirical, beta_vector):
         n_samples = tf.shape(U)[0]
-        alpha =     self.lambda1 + (beta_vector/self.cost_lambda1) * Z_empirical * (tf.reshape(U[:,0],[n_samples,1])-tf.reshape(U[:,1],[n_samples,1]))
+        alpha = self.lambda1 + 0.5 * (beta_vector / self.cost_lambda1) * Z_empirical * (tf.reshape(U[:, 0], [n_samples, 1]) -tf.reshape(U[:, 1], [n_samples, 1]))
         return alpha
